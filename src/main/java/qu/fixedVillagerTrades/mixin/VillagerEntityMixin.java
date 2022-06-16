194601661c20239bbefeed04b082decfde03231a
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 01:39:06 -0400

    removed unneeded code

--- src/main/java/qu/fixedVillagerTrades/mixin/VillagerEntityMixin.java
+++ src/main/java/qu/fixedVillagerTrades/mixin/VillagerEntityMixin.java
@@ -3,18 +3,14 @@ package qu.fixedVillagerTrades.mixin;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.village.VillagerData;
 import net.minecraft.village.VillagerProfession;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VillagerEntity.class)
 public abstract class VillagerEntityMixin extends MerchantEntityMixin {
-
-    @Shadow public abstract VillagerData getVillagerData();
 
     @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
     private void writeNbt(NbtCompound nbt, CallbackInfo ci) {

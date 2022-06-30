400f641f2f84f3fc974c510155bf7dfb4c48f361
BuildTools <unconfigured@null.spigotmc.org>
2022-06-30 17:05:19 -0400

    Config functionality

--- src/main/java/qu/fixedVillagerTrades/FixedVillagerTrades.java
+++ src/main/java/qu/fixedVillagerTrades/FixedVillagerTrades.java
@@ -3,6 +3,7 @@ package qu.fixedVillagerTrades;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import qu.fixedVillagerTrades.config.ModConfig;
 
 public class FixedVillagerTrades implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -12,12 +13,21 @@ public class FixedVillagerTrades implements ModInitializer {
 	public static final String MOD_ID = "fixed-villager-trades";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	private static final ModConfig MOD_CONFIG = new ModConfig();
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Finished Initializing " + MOD_ID);
+	}
+
+	public static boolean areTradesFixed() {
+		return MOD_CONFIG.FIX_TRADES;
+	}
+
+	public static float getMaxDiscount() {
+		return MOD_CONFIG.MAX_DISCOUNT;
 	}
 }
---
+++ src/main/java/qu/fixedVillagerTrades/config/ModConfig.java
@@ -0,0 +1,43 @@
+package qu.fixedVillagerTrades.config;
+
+import com.electronwill.nightconfig.core.ConfigSpec;
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.file.FileConfig;
+import net.fabricmc.loader.api.FabricLoader;
+import qu.fixedVillagerTrades.FixedVillagerTrades;
+
+import java.util.Arrays;
+
+public class ModConfig {
+
+    public final boolean FIX_TRADES;
+    public final float MAX_DISCOUNT;
+
+    public ModConfig() {
+        FixedVillagerTrades.LOGGER.info("Reading config file for " + FixedVillagerTrades.MOD_ID);
+        CommentedFileConfig config = CommentedFileConfig.of(FabricLoader.getInstance().getConfigDir().resolve(FixedVillagerTrades.MOD_ID + ".toml"));
+        config.load();
+        checkConfig(config);
+        FIX_TRADES = config.get("FixedTrades");
+        MAX_DISCOUNT = ((Double)config.get("MaxDiscount")).floatValue();
+        config.close();
+        FixedVillagerTrades.LOGGER.info("Fixed Trades: " + FIX_TRADES + ", Max Discount: " + MAX_DISCOUNT);
+    }
+
+    private static void checkConfig(CommentedFileConfig config) {
+        ConfigSpec spec = new ConfigSpec();
+        spec.defineInList("FixedTrades", true, Arrays.asList(true, false));
+        spec.defineInRange("MaxDiscount", 0.5, 0.0, 1.0);
+        if (!spec.isCorrect(config)) {
+            FixedVillagerTrades.LOGGER.error("One or more config settings were incorrect, setting to default value(s)");
+            config.setComment("FixedTrades", "Whether villager trades are fixed to their profession upon generation. Must be either true or false.");
+            config.setComment("MaxDiscount", "The maximum discount a trade can have. Vanilla Minecraft is equivalent to 1.0. Must be between 0.0 and 1.0.");
+            ConfigSpec.CorrectionListener listener = (action, path, incorrectValue, correctedValue) -> {
+                String pathString = String.join(",", path);
+                FixedVillagerTrades.LOGGER.error("Corrected " + pathString + ": was " + incorrectValue + ", is now " + correctedValue);
+            };
+            spec.correct(config, listener);
+            config.save();
+        }
+    }
+}
